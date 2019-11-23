bdbb4afee6b5b8a714ac1d39cb32c77192ad5ac3
v-v-d <v-v-d@bk.ru>
2019-11-23 15:52:49 +0300

    HomePageSlider.vue without logic added to ./src/components/elements

---
+++ src/components/elements/HomePageSlider.vue
@@ -0,0 +1,99 @@
+<template>
+  <section class="slider">
+    <div class="slider-back content">
+      <img class="slider-img" src="../../assets/slider.jpg">
+      <div class="slider-text">
+        <h2>THE BRAND</h2>
+        <h3>OF LUXERIOUS <span class="color-accent">FASHION</span></h3>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'HomePageSlider',
+  };
+</script>
+
+<style lang="sass">
+  $colorAccent: #f16d7f
+  $darkColorAccent: #222222
+  $lightColorAccent: #ffffff
+
+  .tracking-in-contract-bck
+    -webkit-animation: tracking-in-contract-bck 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both
+    animation: tracking-in-contract-bck 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both
+
+  @-webkit-keyframes tracking-in-contract-bck
+    0%
+      letter-spacing: 1em
+      -webkit-transform: translateZ(400px)
+      transform: translateZ(400px)
+      opacity: 0
+    40%
+      opacity: 0.6
+    100%
+      -webkit-transform: translateZ(0)
+      transform: translateZ(0)
+      opacity: 1
+    @keyframes tracking-in-contract-bck
+      0%
+        letter-spacing: 1em
+        -webkit-transform: translateZ(400px)
+        transform: translateZ(400px)
+        opacity: 0
+    40%
+      opacity: 0.6
+    100%
+      -webkit-transform: translateZ(0)
+      transform: translateZ(0)
+      opacity: 1
+
+  .content
+    width: 1140px
+    margin: 0 auto
+
+  .slider
+    background: #e8e8e8
+    height: 614px
+
+  .slider-back
+    height: 100%
+    display: flex
+    align-items: center
+
+  .slider-img
+    height: 100%
+
+  .slider-text
+    @extend .tracking-in-contract-bck
+
+  .slider-text
+    h2
+      color: $darkColorAccent
+      font-size: 60px
+      font-weight: 900
+      transform: scaleY(1.0009)
+
+  .slider-text
+    h3
+      color: $darkColorAccent
+      font-size: 40px
+      transform: scaleY(1.0009)
+
+  .slider-text
+    position: absolute
+
+  .slider-text:before
+    content: ''
+    background-color: $colorAccent
+    width: 12px
+    height: 75%
+    left: -20px
+    bottom: 10px
+    position: absolute
+
+  .color-accent
+    color: $colorAccent
+</style>
--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -2,18 +2,21 @@
   <div>
     <Header/>
     <NavBar/>
+    <HomePageSlider />
   </div>
 </template>
 
 <script>
   import Header from '../elements/Header.vue';
   import NavBar from '../elements/NavBar.vue';
+  import HomePageSlider from '../elements/HomePageSlider.vue';
 
   export default {
     name: 'HomePage',
     components: {
       Header,
       NavBar,
+      HomePageSlider,
     }
   };
 </script>
