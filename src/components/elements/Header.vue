2500f4cfd83b2fa8d75995df8c918ab6aebc3298
v-v-d <v-v-d@bk.ru>
2019-11-23 20:47:13 +0300

    Styles for logo deleted from Header.vue

--- src/components/elements/Header.vue
+++ src/components/elements/Header.vue
@@ -70,21 +70,6 @@
     align-items: center
     height: 100%
 
-  .logo
-    display: flex
-    align-items: center
-
-  .logo-img
-    margin-right: 15px
-
-  .brand-name
-    font-size: 27px
-    line-height: 32px
-    color: $darkColorAccent
-    display: flex
-    margin-top: 5px
-    font-weight: 100
-
   .color-accent
     color: $colorAccent
     font-weight: 600
