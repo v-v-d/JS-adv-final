7680174ecddfb6a871978c6519dd4d3307c7f386
v-v-d <v-v-d@bk.ru>
2019-11-24 14:54:15 +0300

    CatalogTitleCategory.vue without logic added to ./src/components/elements

---
+++ src/components/elements/CatalogTitleCategory.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="title-category">
+    <div class="content title-category-flex">
+      <h2>New Arrivals</h2>
+      <div class="crumb">
+        <a href="#">Home</a>
+        <a href="#">Men</a>
+        <p class="color-accent">NEW ARRIVALS</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'CatalogTitleCategory',
+  };
+</script>
+
+<style lang="sass">
+  $colorAccent: #f16d7f
+  $darkColorAccent: #222222
+  $lightColorAccent: #ffffff
+
+  .title-category-flex
+    display: flex
+    justify-content: space-between
+    align-items: center
+
+  .title-category
+    background: #f8f3f4
+    margin-bottom: 50px
+    padding: 50px
+    box-sizing: border-box
+
+  .title-category
+    h2
+      color: $colorAccent
+      font-size: 24px
+      font-weight: 900
+      text-transform: uppercase
+
+  .title-category
+    .crumb
+      display: flex
+
+  .crumb
+    a:after
+      content: ' /'
+
+  .crumb
+    a:hover
+      color: $colorAccent
+
+  .crumb a
+    color: #636363
+
+  .crumb
+    display: block
+    padding: 2px
+    color: #636363
+    font-size: 14px
+    text-transform: uppercase
+</style>
--- src/components/pages/CatalogPage.vue
+++ src/components/pages/CatalogPage.vue
@@ -1,10 +1,26 @@
 <template>
-  <h1>Это CatalogPage.vue</h1>
+  <div>
+    <Header/>
+    <NavBar/>
+    <CatalogTitleCategory/>
+    <Footer />
+  </div>
 </template>
 
 <script>
+  import Header from '../elements/Header.vue';
+  import NavBar from '../elements/NavBar.vue';
+  import CatalogTitleCategory from '../elements/CatalogTitleCategory.vue';
+  import Footer from '../elements/Footer.vue';
+
   export default {
     name: 'CatalogPage',
+    components: {
+      Header,
+      NavBar,
+      CatalogTitleCategory,
+      Footer,
+    },
   };
 </script>
 
