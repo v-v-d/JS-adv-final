43e3df54735d6173e64ba3697289c208a5fea6fb
v-v-d <v-v-d@bk.ru>
2019-11-26 00:01:24 +0300

    CheckoutOrdering.vue without logic added to ./src/components/elements

---
+++ src/components/elements/CheckoutOrdering.vue
@@ -0,0 +1,389 @@
+<template>
+  <div class="stages-of-ordering content">
+
+    <div class="stages-of-ordering-item">
+      <details>
+        <summary>01. Shipping Adress</summary>
+        <div class="root-box">
+          <div class="box">
+            <form>
+              <h3>Check as a guest or register</h3>
+              <p>Register with us for future convenience</p>
+              <label>
+                <input type="radio" name="order-type" value="checkout-as-guest">
+                checkout as guest
+              </label><br>
+              <label>
+                <input type="radio" name="order-type" value="register">
+                register
+              </label>
+
+              <h3>register and save time!</h3>
+              <p>Register with us for future convenience</p>
+              <p>Fast and easy checkout</p>
+              <p>Easy access to your order history and status</p>
+              <button type="button">Continue</button>
+            </form>
+          </div>
+          <div class="box">
+            <form class="form-authorization">
+              <h3>Already registed?</h3>
+              <p>Please log in below</p>
+              <label><p class="required-input">EMAIL ADDRESS</p>
+                <input required type="email">
+              </label>
+              <label><p class="required-input">PASSWORD</p>
+                <input required type="password">
+              </label>
+
+              <p class="important-message">* Required Fileds</p>
+              <button type="button">Log in</button>
+              <a class="forgot-password" href="#">Forgot Password ?</a>
+            </form>
+          </div>
+        </div>
+
+      </details>
+    </div>
+    <div class="stages-of-ordering-item">
+      <details>
+        <summary>02. BILLING INFORMATION</summary>
+        <div class="root-box">
+          <div class="box">
+            <form>
+              <h3>Check as a guest or register</h3>
+              <p>Register with us for future convenience</p>
+              <label>
+                <input type="radio" name="order-type" value="checkout-as-guest">
+                checkout as guest
+              </label><br>
+              <label>
+                <input type="radio" name="order-type" value="register">
+                register
+              </label>
+
+              <h3>register and save time!</h3>
+              <p>Register with us for future convenience</p>
+              <p>Fast and easy checkout</p>
+              <p>Easy access to your order history and status</p>
+              <button type="button">Continue</button>
+            </form>
+          </div>
+          <div class="box">
+            <form class="form-authorization">
+              <h3>Already registed?</h3>
+              <p>Please log in below</p>
+              <label><p class="required-input">EMAIL ADDRESS</p>
+                <input required type="email">
+              </label>
+              <label><p class="required-input">PASSWORD</p>
+                <input required type="password">
+              </label>
+
+              <p class="important-message">* Required Fileds</p>
+              <button type="button">Log in</button>
+              <a class="forgot-password" href="#">Forgot Password ?</a>
+            </form>
+          </div>
+        </div>
+
+      </details>
+    </div>
+    <div class="stages-of-ordering-item">
+      <details>
+        <summary>03. SHIPPING INFORMATION</summary>
+        <div class="root-box">
+          <div class="box">
+            <form>
+              <h3>Check as a guest or register</h3>
+              <p>Register with us for future convenience</p>
+              <label>
+                <input type="radio" name="order-type" value="checkout-as-guest">
+                checkout as guest
+              </label><br>
+              <label>
+                <input type="radio" name="order-type" value="register">
+                register
+              </label>
+
+              <h3>register and save time!</h3>
+              <p>Register with us for future convenience</p>
+              <p>Fast and easy checkout</p>
+              <p>Easy access to your order history and status</p>
+              <button type="button">Continue</button>
+            </form>
+          </div>
+          <div class="box">
+            <form class="form-authorization">
+              <h3>Already registed?</h3>
+              <p>Please log in below</p>
+              <label><p class="required-input">EMAIL ADDRESS</p>
+                <input required type="email">
+              </label>
+              <label><p class="required-input">PASSWORD</p>
+                <input required type="password">
+              </label>
+
+              <p class="important-message">* Required Fileds</p>
+              <button type="button">Log in</button>
+              <a class="forgot-password" href="#">Forgot Password ?</a>
+            </form>
+          </div>
+        </div>
+
+      </details>
+    </div>
+    <div class="stages-of-ordering-item">
+      <details>
+        <summary>04. SHIPPING METHOD</summary>
+        <div class="root-box">
+          <div class="box">
+            <form>
+              <h3>Check as a guest or register</h3>
+              <p>Register with us for future convenience</p>
+              <label>
+                <input type="radio" name="order-type" value="checkout-as-guest">
+                checkout as guest
+              </label><br>
+              <label>
+                <input type="radio" name="order-type" value="register">
+                register
+              </label>
+
+              <h3>register and save time!</h3>
+              <p>Register with us for future convenience</p>
+              <p>Fast and easy checkout</p>
+              <p>Easy access to your order history and status</p>
+              <button type="button">Continue</button>
+            </form>
+          </div>
+          <div class="box">
+            <form class="form-authorization">
+              <h3>Already registed?</h3>
+              <p>Please log in below</p>
+              <label><p class="required-input">EMAIL ADDRESS</p>
+                <input required type="email">
+              </label>
+              <label><p class="required-input">PASSWORD</p>
+                <input required type="password">
+              </label>
+
+              <p class="important-message">* Required Fileds</p>
+              <button type="button">Log in</button>
+              <a class="forgot-password" href="#">Forgot Password ?</a>
+            </form>
+          </div>
+        </div>
+
+      </details>
+    </div>
+    <div class="stages-of-ordering-item">
+      <details>
+        <summary>05. PAYMENT METHOD</summary>
+        <div class="root-box">
+          <div class="box">
+            <form>
+              <h3>Check as a guest or register</h3>
+              <p>Register with us for future convenience</p>
+              <label>
+                <input type="radio" name="order-type" value="checkout-as-guest">
+                checkout as guest
+              </label><br>
+              <label>
+                <input type="radio" name="order-type" value="register">
+                register
+              </label>
+
+              <h3>register and save time!</h3>
+              <p>Register with us for future convenience</p>
+              <p>Fast and easy checkout</p>
+              <p>Easy access to your order history and status</p>
+              <button type="button">Continue</button>
+            </form>
+          </div>
+          <div class="box">
+            <form class="form-authorization">
+              <h3>Already registed?</h3>
+              <p>Please log in below</p>
+              <label><p class="required-input">EMAIL ADDRESS</p>
+                <input required type="email">
+              </label>
+              <label><p class="required-input">PASSWORD</p>
+                <input required type="password">
+              </label>
+
+              <p class="important-message">* Required Fileds</p>
+              <button type="button">Log in</button>
+              <a class="forgot-password" href="#">Forgot Password ?</a>
+            </form>
+          </div>
+        </div>
+
+      </details>
+    </div>
+    <div class="stages-of-ordering-item">
+      <details>
+        <summary>06. ORDER REVIEW</summary>
+        <div class="root-box">
+          <div class="box">
+            <form>
+              <h3>Check as a guest or register</h3>
+              <p>Register with us for future convenience</p>
+              <label>
+                <input type="radio" name="order-type" value="checkout-as-guest">
+                checkout as guest
+              </label><br>
+              <label>
+                <input type="radio" name="order-type" value="register">
+                register
+              </label>
+
+              <h3>register and save time!</h3>
+              <p>Register with us for future convenience</p>
+              <p>Fast and easy checkout</p>
+              <p>Easy access to your order history and status</p>
+              <button type="button">Continue</button>
+            </form>
+          </div>
+          <div class="box">
+            <form class="form-authorization">
+              <h3>Already registed?</h3>
+              <p>Please log in below</p>
+              <label><p class="required-input">EMAIL ADDRESS</p>
+                <input required type="email">
+              </label>
+              <label><p class="required-input">PASSWORD</p>
+                <input required type="password">
+              </label>
+
+              <p class="important-message">* Required Fileds</p>
+              <button type="button">Log in</button>
+              <a class="forgot-password" href="#">Forgot Password ?</a>
+            </form>
+          </div>
+        </div>
+
+      </details>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CheckoutOrdering',
+  };
+</script>
+
+<style lang="sass">
+  $colorAccent: #f16d7f
+  $darkColorAccent: #222222
+  $lightColorAccent: #ffffff
+
+  .root-box
+    display: flex
+    margin: 0 30px
+
+  .root-box
+    .box
+      width: 50%
+
+  .stages-of-ordering
+    margin-bottom: 40px
+
+  .stages-of-ordering-item summary
+    color: $darkColorAccent
+    font-size: 16px
+    font-weight: 700
+    text-transform: uppercase
+    margin: 25px 0
+
+  .stages-of-ordering-item
+    details[open] > summary::before
+      content: ""
+
+  .stages-of-ordering-item
+    details
+      border-bottom: 1px solid #e8e8e8
+      margin-bottom: 10px
+
+  .stages-of-ordering-item h3,
+  .stages-of-ordering-item a
+    color: $darkColorAccent
+    font-size: 13px
+    margin-bottom: 10px
+    text-transform: uppercase
+
+  .stages-of-ordering-item a
+    margin-left: 10px
+
+  .stages-of-ordering-item
+    a:hover
+      color: $colorAccent
+
+  .stages-of-ordering-item p
+    color: #b3b2b2
+    font-size: 13px
+    font-weight: 400
+    line-height: 20px
+    margin: 10px 0
+
+  .stages-of-ordering-item
+    button
+      color: #4a4a4a
+      font-size: 14px
+      font-weight: 700
+      border: 1px solid #eaeaea
+      box-sizing: border-box
+      background-color: $lightColorAccent
+      line-height: 50px
+      padding: 0 50px
+      margin: 20px 0
+
+  .stages-of-ordering-item
+    input
+      margin: 5px 0
+
+  .stages-of-ordering-item input[type = 'email'],
+  .stages-of-ordering-item input[type = 'password']
+    border: 1px solid #eaeaea
+    box-sizing: border-box
+    background-color: $lightColorAccent
+    width: 390px
+    line-height: 45px
+    padding: 0 10px
+
+  .stages-of-ordering-item input:focus
+    outline-color: $colorAccent
+
+  .stages-of-ordering-item
+    summary:focus
+      outline-color: transparent
+
+  .stages-of-ordering-item
+    summary:hover
+      color: $colorAccent
+
+  .stages-of-ordering-item
+    input[type = 'radio']
+      margin-bottom: 20px
+
+  .stages-of-ordering-item
+    button:hover
+      background: $colorAccent
+      color: $lightColorAccent
+
+  p.required-input
+    font-size: 12px
+    line-height: 20px
+    color: $darkColorAccent
+
+  .required-input:after
+    content: '*'
+    color: #ff0d0d
+
+  .stages-of-ordering-item
+    .important-message
+      color: #ff0d0d
+      margin: 10px 0
+</style>
