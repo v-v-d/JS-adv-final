f4db878969f0b2412b973632535905f212014c93
v-v-d <v-v-d@bk.ru>
2019-11-25 15:32:18 +0300

    header, navbar, footer added to ProductPage.vue

--- src/components/pages/ProductPage.vue
+++ src/components/pages/ProductPage.vue
@@ -1,10 +1,23 @@
 <template>
-  <h1>Это ProductPage.vue</h1>
+  <div>
+    <Header/>
+    <NavBar/>
+    <Footer/>
+  </div>
 </template>
 
 <script>
+  import Header from '../elements/Header.vue';
+  import NavBar from '../elements/NavBar.vue';
+  import Footer from '../elements/Footer.vue';
+
   export default {
     name: 'ProductPage',
+    components: {
+      Header,
+      NavBar,
+      Footer,
+    }
   };
 </script>
 
