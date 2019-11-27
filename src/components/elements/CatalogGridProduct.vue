77a0744d3879d898460168248f3d21be82348a2a
v-v-d <v-v-d@bk.ru>
2019-11-27 17:53:10 +0300

    CatalogGridProduct.vue duplicate styles deleted

--- src/components/elements/CatalogGridProduct.vue
+++ src/components/elements/CatalogGridProduct.vue
@@ -387,72 +387,6 @@
     flex-wrap: wrap
     justify-content: space-between
 
-  .product-item
-    width: 261px
-    position: relative
-    margin-bottom: 40px
-
-  .img-product
-    overflow: hidden
-    width: 261px
-    height: 279px
-    position: relative
-    transition: .3s
-
-  .product-item:hover
-    img
-      transform: scale(1.1) rotate(2deg)
-
-  .product-description
-    padding: 7px 20px
-
-  .product-name
-    color: $darkColorAccent
-    font-size: 13px
-    text-transform: uppercase
-    padding: 7px 0
-
-  .product-price
-    color: $colorAccent
-    font-size: 16px
-    font-weight: 700
-    padding: 7px 0
-
-  .flex-center
-    width: 261px
-    display: flex
-    justify-content: center
-
-  .button-add-to-cart
-    border: 1px solid $lightColorAccent
-    box-sizing: border-box
-    line-height: 39px
-    color: $lightColorAccent
-    padding: 0 13px 0 13px
-    position: absolute
-    top: 89px
-    display: none
-    @extend .flip-in-ver-left
-    transition: .2s
-
-  .button-add-to-cart-img
-    width: 20px
-    margin-right: 3px
-    transform: none !important
-
-  .product-item:hover
-    box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.16)
-
-  .product-item:hover
-    .button-add-to-cart
-      display: flex
-
-  .product-item:hover .img-product
-    filter: brightness(50%)
-
-  .button-add-to-cart:hover
-    background: $colorAccent
-
   .product-nav
     display: flex
     justify-content: space-between
@@ -496,4 +430,7 @@
   .btn-view-all:hover
     color: $lightColorAccent
     background: $colorAccent
+
+  .product-item:nth-child(3n-1):last-child
+    margin-right: 34.73%
 </style>
